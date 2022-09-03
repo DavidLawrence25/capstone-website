2572910c6da5e29c8ce993c4fbf14b3db921197e
Aaron Night <mdcartkids@gmail.com>
2022-09-03 11:10:22 -0600

    Simple Starting Text

--- scripts/indexConsoleText.js
+++ scripts/indexConsoleText.js
@@ -1,36 +1,40 @@
-let canWrite = true;
 let consoleContents = document.getElementById(`indexConsole`).innerHTML;
+let messagesSent = 0;
 
-async function Wait(ms) {
-    await new Promise(r => setTimeout(r, ms));
+async function ClearConsole(messageID) {
+    while (messageID != messagesSent) { await new Promise(r => setTimeout(r, 10)); }
+    document.getElementById(`indexConsole`).innerHTML = consoleContents = ``;
+    messagesSent++;
 }
 
-async function TypeString(str, outputStr, charsPerSec, breakAfterComplete) {
-    while (canWrite == false) { await new Promise(r => setTimeout(r, 100)); }
-    canWrite = false;
+async function TypeString(str, charsPerSec, breakAfterComplete, messageID) {
+    while (messageID != messagesSent) { await new Promise(r => setTimeout(r, 10)); }
     for (let i = 0; i < str.length; i++) {
-        outputStr += str.charAt(i);
-        document.getElementById(`indexConsole`).innerHTML = outputStr;
+        consoleContents += str.charAt(i);
+        document.getElementById(`indexConsole`).innerHTML = consoleContents;
         await new Promise(r => setTimeout(r, 1000 / charsPerSec));
     }
+    consoleContents += `<br>`;
     await new Promise(r => setTimeout(r, breakAfterComplete));
-    canWrite = true;
+    messagesSent++;
 }
 
-async function TypeLines(arr, outputStr, linesPerSec, clearField) {
-    while (canWrite == false) { await new Promise(r => setTimeout(r, 100));; }
-    canWrite = false;
+async function TypeLines(arr, linesPerSec, clearField, messageID) {
+    while (messageID != messagesSent) { await new Promise(r => setTimeout(r, 10));; }
     for (let i = 0; i < arr.length; i++) {
         if (clearField) {
-            outputStr = `${arr[i]}<br>`;
+            consoleContents = `${arr[i]}<br>`;
         } else {
-            outputStr += `${arr[i]}<br>`;
+            consoleContents += `${arr[i]}<br>`;
         }
-        document.getElementById(`indexConsole`).innerHTML = outputStr;
+        document.getElementById(`indexConsole`).innerHTML = consoleContents;
         await new Promise(r => setTimeout(r, 1000 / linesPerSec));
     }
-    canWrite = true;
+    messagesSent++;
 }
 
-TypeString(`Welcome to my Capstone Project Homepage!`, consoleContents, 20, 1000);
-TypeLines([`&emsp;> Fetching student data.`, `&emsp;> Fetching student data..`, `&emsp;> Fetching student data...`, `&emsp;> Student data retrieved.`], consoleContents, 3, true);
+TypeString(`Welcome to my Capstone Project Homepage!`, 20, 1000, 0);
+TypeLines([`&emsp;> Fetching student data.`, `&emsp;> Fetching student data..`, `&emsp;> Fetching student data...`, `&emsp;> Student data retrieved.`], 3, true, 1);
+ClearConsole(2);
+TypeString(`Sophomore - Class of 2025`, 250, 0, 3);
+TypeString(`My name is David Lawrence, and I love to create and discover things. While I don't have time to do it much these days, I like to draw and play creative games like Animal Crossing. Mathematics and programming are my main focus these days, as they are high-paying fields that go hand-in-hand.`, 500, 1000, 4);
